9287e98c99801d66768006a5e6f669f0480c0131
Bruno Gemelli <bgemelli@interactone.com>
2021-04-14 22:02:48 -0300

    primeiro modulo

---
+++ app/code/GamaAcademy/SampleModule/registration.php
@@ -0,0 +1,5 @@
+<?php
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'GamaAcademy_SampleModule', __DIR__);
--- app/etc/config.php
+++ app/etc/config.php
@@ -333,6 +333,7 @@ return [
         'Dotdigitalgroup_Email' => 1,
         'Dotdigitalgroup_Chat' => 1,
         'Dotdigitalgroup_Sms' => 1,
+        'GamaAcademy_SampleModule' => 1,
         'Klarna_Core' => 1,
         'Klarna_Ordermanagement' => 1,
         'Klarna_Kp' => 1,
