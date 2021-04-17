3f26a71982629ef1cb6c3367275870c9fd184056
Bruno Gemelli <bgemelli@interactone.com>
2021-04-17 18:00:12 -0300

    Arquivo para uso com testes de DI

---
+++ app/code/GamaAcademy/ModuleConfigDemo/Model/Product.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace GamaAcademy\ModuleConfigDemo\Model;
+
+use Magento\Catalog\Api\CategoryRepositoryInterface;
+use Magento\Catalog\Model\FilterProductCustomAttribute;
+use Magento\Catalog\Model\Product\Attribute\Backend\Media\EntryConverterPool;
+use Magento\Catalog\Model\Product\Attribute\Source\Status;
+use Magento\Framework\Api\AttributeValueFactory;
+
+class Product extends \Magento\Catalog\Model\Product
+{
+    public function __construct(
+        \Magento\Framework\Model\Context $context,
+        \Magento\Framework\Registry $registry,
+        \Magento\Framework\Api\ExtensionAttributesFactory $extensionFactory,
+        AttributeValueFactory $customAttributeFactory,
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        \Magento\Catalog\Api\ProductAttributeRepositoryInterface $metadataService,
+        \Magento\Catalog\Model\Product\Url $url,
+        \Magento\Catalog\Model\Product\Link $productLink,
+        \Magento\Catalog\Model\Product\Configuration\Item\OptionFactory $itemOptionFactory,
+        \Magento\CatalogInventory\Api\Data\StockItemInterfaceFactory $stockItemFactory,
+        \Magento\Catalog\Model\Product\OptionFactory $catalogProductOptionFactory,
+        \Magento\Catalog\Model\Product\Visibility $catalogProductVisibility,
+        \Magento\Catalog\Model\Product\Attribute\Source\Status $catalogProductStatus,
+        \Magento\Catalog\Model\Product\Media\Config $catalogProductMediaConfig,
+        \Magento\Catalog\Model\Product\Type $catalogProductType,
+        \Magento\Framework\Module\Manager $moduleManager,
+        \Magento\Catalog\Helper\Product $catalogProduct,
+        \Magento\Catalog\Model\ResourceModel\Product $resource,
+        \Magento\Catalog\Model\ResourceModel\Product\Collection $resourceCollection,
+        \Magento\Framework\Data\CollectionFactory $collectionFactory,
+        \Magento\Framework\Filesystem $filesystem,
+        \Magento\Framework\Indexer\IndexerRegistry $indexerRegistry,
+        \Magento\Catalog\Model\Indexer\Product\Flat\Processor $productFlatIndexerProcessor,
+        \Magento\Catalog\Model\Indexer\Product\Price\Processor $productPriceIndexerProcessor,
+        \Magento\Catalog\Model\Indexer\Product\Eav\Processor $productEavIndexerProcessor,
+        CategoryRepositoryInterface $categoryRepository,
+        \Magento\Catalog\Model\Product\Image\CacheFactory $imageCacheFactory,
+        \Magento\Catalog\Model\ProductLink\CollectionProvider $entityCollectionProvider,
+        \Magento\Catalog\Model\Product\LinkTypeProvider $linkTypeProvider,
+        \Magento\Catalog\Api\Data\ProductLinkInterfaceFactory $productLinkFactory,
+        \Magento\Catalog\Api\Data\ProductLinkExtensionFactory $productLinkExtensionFactory,
+        EntryConverterPool $mediaGalleryEntryConverterPool,
+        \Magento\Framework\Api\DataObjectHelper $dataObjectHelper,
+        \Magento\Framework\Api\ExtensionAttribute\JoinProcessorInterface $joinProcessor,
+        array $data = [],
+        \Magento\Eav\Model\Config $config = null,
+        FilterProductCustomAttribute $filterCustomAttribute = null
+    ) {
+        parent::__construct($context, $registry, $extensionFactory, $customAttributeFactory, $storeManager, $metadataService, $url, $productLink, $itemOptionFactory, $stockItemFactory, $catalogProductOptionFactory, $catalogProductVisibility, $catalogProductStatus, $catalogProductMediaConfig, $catalogProductType, $moduleManager, $catalogProduct, $resource, $resourceCollection, $collectionFactory, $filesystem, $indexerRegistry, $productFlatIndexerProcessor, $productPriceIndexerProcessor, $productEavIndexerProcessor, $categoryRepository, $imageCacheFactory, $entityCollectionProvider, $linkTypeProvider, $productLinkFactory, $productLinkExtensionFactory, $mediaGalleryEntryConverterPool, $dataObjectHelper, $joinProcessor, $data, $config, $filterCustomAttribute);
+    }
+
+    public function getName()
+    {
+        return 'BRUNO TESTE - ' . $this->_getData(self::NAME);
+    }
+
+}
