f6e100be69f057bd3612d2c272cb591dd245321c
Bruno Gemelli <bgemelli@interactone.com>
2021-04-21 20:48:24 -0300

    Layout / visual examples

--- app/code/GamaAcademy/ModuleConfigDemo/Block/ScreenExample.php
+++ app/code/GamaAcademy/ModuleConfigDemo/Block/ScreenExample.php
@@ -6,6 +6,17 @@ use Magento\Framework\View\Element\Template;
 
 class ScreenExample extends Template
 {
+//    protected $_template = 'GamaAcademy_ModuleConfigDemo::screen.phtml';
+
+    /**
+     * @param string $template
+     * @return ScreenExample
+     */
+    public function setTemplate($template)
+    {
+        return parent::setTemplate($template);
+//        return 'GamaAcademy_ModuleConfigDemo::screen.phtml';
+    }
 
     /**
      * @return mixed
