6441629e6cf6146b5020ddcd0337b7e289b87750
Bruno Gemelli <bgemelli@interactone.com>
2021-04-24 17:15:10 -0300

    UI Components

---
+++ app/code/GamaAcademy/Vacina/Ui/Component/DataProvider.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace GamaAcademy\Vacina\Ui\Component;
+
+use Magento\Framework\Api\Filter;
+use Magento\Framework\Api\FilterBuilder;
+use Magento\Framework\Api\Search\SearchCriteriaBuilder;
+use Magento\Framework\App\ObjectManager;
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\AuthorizationInterface;
+use Magento\Framework\View\Element\UiComponent\DataProvider\Reporting;
+
+/**
+ * DataProvider for vacina ui.
+ */
+class DataProvider extends \Magento\Framework\View\Element\UiComponent\DataProvider\DataProvider
+{
+    /**
+     * @var AuthorizationInterface
+     */
+    private $authorization;
+
+    /**
+     * @var AddFilterInterface[]
+     */
+    private $additionalFilterPool;
+
+    /**
+     * @param string $name
+     * @param string $primaryFieldName
+     * @param string $requestFieldName
+     * @param Reporting $reporting
+     * @param SearchCriteriaBuilder $searchCriteriaBuilder
+     * @param RequestInterface $request
+     * @param FilterBuilder $filterBuilder
+     * @param array $meta
+     * @param array $data
+     * @param array $additionalFilterPool
+     * @SuppressWarnings(PHPMD.ExcessiveParameterList)
+     */
+    public function __construct(
+        $name,
+        $primaryFieldName,
+        $requestFieldName,
+        Reporting $reporting,
+        SearchCriteriaBuilder $searchCriteriaBuilder,
+        RequestInterface $request,
+        FilterBuilder $filterBuilder,
+        AuthorizationInterface $authorization,
+        array $meta = [],
+        array $data = [],
+        array $additionalFilterPool = []
+    ) {
+        parent::__construct(
+            $name,
+            $primaryFieldName,
+            $requestFieldName,
+            $reporting,
+            $searchCriteriaBuilder,
+            $request,
+            $filterBuilder,
+            $meta,
+            $data
+        );
+
+        $this->meta = array_replace_recursive($meta, $this->prepareMetadata());
+        $this->additionalFilterPool = $additionalFilterPool;
+        $this->authorization = $authorization;
+    }
+
+    /**
+     * Prepares Meta
+     *
+     * @return array
+     */
+    public function prepareMetadata()
+    {
+        $metadata = [];
+
+        $metadata = [
+            'cms_page_columns' => [
+                'arguments' => [
+                    'data' => [
+                        'config' => [
+                            'editorConfig' => [
+                                'enabled' => false
+                            ],
+                            'componentType' => \Magento\Ui\Component\Container::NAME
+                        ]
+                    ]
+                ]
+            ]
+        ];
+
+        return $metadata;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function addFilter(Filter $filter)
+    {
+        if (!empty($this->additionalFilterPool[$filter->getField()])) {
+            $this->additionalFilterPool[$filter->getField()]->addFilter($this->searchCriteriaBuilder, $filter);
+        } else {
+            parent::addFilter($filter);
+        }
+    }
+}
---
+++ app/code/GamaAcademy/Vacina/Ui/Component/Listing/Column/VacinaActions.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace GamaAcademy\Vacina\Ui\Component\Listing\Column;
+
+use Magento\Framework\UrlInterface;
+use Magento\Framework\View\Element\UiComponent\ContextInterface;
+use Magento\Framework\View\Element\UiComponentFactory;
+use Magento\Ui\Component\Listing\Columns\Column;
+use Magento\Framework\App\ObjectManager;
+use Magento\Framework\Escaper;
+
+/**
+ * Class to build edit and delete link for each item.
+ */
+class VacinaActions extends Column
+{
+    /**
+     * Url path
+     */
+    const URL_PATH_EDIT = 'gamaacademy/vacina/edit';
+    const URL_PATH_DELETE = 'gamaacademy/vacina/delete';
+    const URL_PATH_DETAILS = 'gamaacademy/vacina/details';
+
+    /**
+     * @var UrlInterface
+     */
+    protected $urlBuilder;
+
+    /**
+     * @var Escaper
+     */
+    private $escaper;
+
+    /**
+     * @param ContextInterface $context
+     * @param UiComponentFactory $uiComponentFactory
+     * @param UrlInterface $urlBuilder
+     * @param array $components
+     * @param array $data
+     */
+    public function __construct(
+        ContextInterface $context,
+        UiComponentFactory $uiComponentFactory,
+        UrlInterface $urlBuilder,
+        Escaper $escaper,
+        array $components = [],
+        array $data = []
+    ) {
+        $this->urlBuilder = $urlBuilder;
+        $this->escaper = $escaper;
+        parent::__construct($context, $uiComponentFactory, $components, $data);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function prepareDataSource(array $dataSource)
+    {
+        if (isset($dataSource['data']['items'])) {
+            foreach ($dataSource['data']['items'] as & $item) {
+                if (isset($item['id'])) {
+                    $title = $this->escaper->escapeHtmlAttr($item['nome']);
+                    $item[$this->getData('name')] = [
+                        'edit' => [
+                            'href' => $this->urlBuilder->getUrl(
+                                static::URL_PATH_EDIT,
+                                [
+                                    'id' => $item['id'],
+                                ]
+                            ),
+                            'label' => __('Edit'),
+                        ],
+                        'delete' => [
+                            'href' => $this->urlBuilder->getUrl(
+                                static::URL_PATH_DELETE,
+                                [
+                                    'id' => $item['id'],
+                                ]
+                            ),
+                            'label' => __('Delete'),
+                            'confirm' => [
+                                'title' => __('Delete %1', $title),
+                                'message' => __('Are you sure you want to delete a %1 record?', $title),
+                            ],
+                            'post' => true,
+                        ],
+                    ];
+                }
+            }
+        }
+
+        return $dataSource;
+    }
+}
